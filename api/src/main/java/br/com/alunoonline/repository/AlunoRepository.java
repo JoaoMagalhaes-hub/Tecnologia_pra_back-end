2ba32d526c4e634c1247bea792a130578fe14d65
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-08-27 21:07:07 -0300

    Extends

--- api/src/main/java/br/com/alunoonline/repository/AlunoRepository.java
+++ api/src/main/java/br/com/alunoonline/repository/AlunoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface AlunoRepository {
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
 }
