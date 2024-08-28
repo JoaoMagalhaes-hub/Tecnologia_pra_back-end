91eee91870b242b65cad2be546c0d0722513ca68
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-08-27 21:02:10 -0300

    Import

--- api/src/main/java/br/com/alunoonline/repository/AlunoRepository.java
+++ api/src/main/java/br/com/alunoonline/repository/AlunoRepository.java
@@ -1,6 +1,7 @@
 package br.com.alunoonline.api.repository;
 
-import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface AlunoRepository {
